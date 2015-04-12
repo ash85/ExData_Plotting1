d938170292183fe662d517ed99800ba4157b0691
aswathmukund <aswathmukund@gmail.com>
2015-04-12 13:25:48 -0700

    project 1 sync

---
+++ Plot4.R
@@ -0,0 +1,37 @@
+setwd("~/github/Exploratory_Data_Analysis/ExData_Plotting1")
+
+## Read the dataset
+data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                 nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+## Subset the data
+data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+
+## Convert dates and concatenate Date Time
+data$Datetime <- as.POSIXct(paste(as.Date(data$Date), data$Time))
+
+
+
+
+## Plot 4
+par(mfrow=c(2,2), mar=c(4,4,0.5,0.5), oma=c(0,1,1,0),cex=0.75)
+with(data, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage ", xlab="datetime")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Energy Sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global Reactive Power (kilowatts)",xlab="datetime")
+})
+
+## Saving to file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
